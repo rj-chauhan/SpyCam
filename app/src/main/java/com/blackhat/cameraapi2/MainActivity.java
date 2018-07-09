f45ab023b44e26d875ee1339b267e8f269105746
Rohit Chauhan <Rohitc9537@gmail.com>
2018-07-09 11:41:05 +0530

    Update MainActivity.java

--- app/src/main/java/com/blackhat/cameraapi2/MainActivity.java
+++ app/src/main/java/com/blackhat/cameraapi2/MainActivity.java
@@ -1,129 +1,42 @@
-package com.blackhat.cameraapi2;
+package com.blackhat.spycam;
 
-
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.blackhat.spycam.SpyListener;
-import com.blackhat.spycam.SpyService;
-import com.blackhat.spycam.SpyServiceImp;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TreeMap;
 
+public class MainActivity extends AppCompatActivity implements SpyListener {
 
-/**
- * App's Main Activity showing a simple usage of the picture taking service.
- *
- * @author hzitoun (zitoun.hamed@gmail.com)
- */
-public class MainActivity extends AppCompatActivity implements SpyListener,
-        ActivityCompat.OnRequestPermissionsResultCallback {
-
-
-    public static final int MY_PERMISSIONS_REQUEST_ACCESS_CODE = 1;
-
-    private ImageView uploadBackPhoto;
-    private ImageView uploadFrontPhoto;
-    private SpyService pictureService;
-
-
+    public SpyService spyService;
+    public Button btn;
+    public ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(Build.VERSION.SDK_INT >=23){
-            checkPermissions();
-        }
-        uploadBackPhoto = (ImageView) findViewById(R.id.backIV);
-        uploadFrontPhoto = (ImageView) findViewById(R.id.frontIV);
-        final Button btn = (Button) findViewById(R.id.startCaptureBtn);
-        pictureService = SpyServiceImp.getInstance(this,"SpyCam");
-        btn.setOnClickListener(v -> {
-                    showToast("Starting capture!");
-                    pictureService.startCapturing(this);
-                }
-        );
-    }
-
-    private void showToast(final String text) {
-        runOnUiThread(() ->
-                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show()
-        );
+        btn = findViewById(R.id.btnCapture);
+        image =findViewById(R.id.image);
+        spyService = SpyServiceImp.getInstance(this,"Spy");
+        btn.setOnClickListener(view -> spyService.startCapturing(MainActivity.this));
     }
 
     @Override
-    public void onDoneCapturingAllPhotos(TreeMap<String, byte[]> picturesTaken) {
-        if (picturesTaken != null && !picturesTaken.isEmpty()) {
-            showToast("Done capturing photo!");
-            return;
-        }
-        showToast("No camera detected!");
+    public void onCaptureDone(String s, byte[] bytes) {
+        Toast.makeText(this, "Captured", Toast.LENGTH_SHORT).show();
+        Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        image.setImageBitmap(Bitmap.createScaledBitmap(bmp, image.getWidth(),
+                image.getHeight(), false));
+
     }
 
     @Override
-    public void onCaptureDone(String pictureUrl, byte[] pictureData) {
-        if (pictureData != null && pictureUrl != null) {
-            runOnUiThread(() -> {
-                final Bitmap bitmap = BitmapFactory.decodeByteArray(pictureData, 0, pictureData.length);
-                final int nh = (int) (bitmap.getHeight() * (512.0 / bitmap.getWidth()));
-                final Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 512, nh, true);
-                if (pictureUrl.contains(".jpg")) {
-                    uploadFrontPhoto.setImageBitmap(scaled);
-                }
-            });
-        }
-    }
+    public void onDoneCapturingAllPhotos(TreeMap<String, byte[]> treeMap) {
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_ACCESS_CODE: {
-                if (!(grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    if(Build.VERSION.SDK_INT >=23){
-                        checkPermissions();
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * checking  permissions at Runtime.
-     */
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void checkPermissions() {
-        final String[] requiredPermissions = {
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA,
-        };
-        final List<String> neededPermissions = new ArrayList<>();
-        for (final String p : requiredPermissions) {
-            if (ContextCompat.checkSelfPermission(getApplicationContext(),
-                    p) != PackageManager.PERMISSION_GRANTED) {
-                neededPermissions.add(p);
-            }
-        }
-        if (!neededPermissions.isEmpty()) {
-            requestPermissions(neededPermissions.toArray(new String[]{}),
-                    MY_PERMISSIONS_REQUEST_ACCESS_CODE);
-        }
     }
 }
-
-
